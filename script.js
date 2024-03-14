6d93d413ff598d18a6586f4d74bab418b39df868
ulascorb_ <107434656+ulascorb321@users.noreply.github.com>
2024-03-14 17:30:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+let photo1 = document.getElementById("photo1");
+let photo2 = document.getElementById("photo2");
+let photo3 = document.getElementById("photo3");
+let photo4 = document.getElementById("photo4");
+let photo5 = document.getElementById("photo5");
+let photo6 = document.getElementById("photo6");
+let photo7 = document.getElementById("photo7");
+let photo8 = document.getElementById("photo8");
+let photo9 = document.getElementById("photo9");
+
+
+photo1.onclick = function () {
+    photo1.setAttribute("id", "photo1Scale");
+}
+
+photo1.ondblclick = function () {
+    photo1.setAttribute("id", "photo1")
+}
+
+photo2.onclick = function () {
+    photo2.setAttribute("id", "photo2Scale");
+}
+
+photo2.ondblclick = function () {
+    photo2.setAttribute("id", "photo2")
+}
+
+photo3.onclick = function () {
+    photo3.setAttribute("id", "photo3Scale");  
+}
+
+photo3.ondblclick = function () {
+    photo3.setAttribute("id", "photo3");
+}
+
+photo4.ondblclick = function () {
+    photo4.setAttribute("id", "photo4")
+}
+
+photo4.onclick = function () {
+    photo4.setAttribute("id", "photo4Scale");
+}
+
+photo5.ondblclick = function () {
+    photo5.setAttribute("id", "photo5")
+}
+
+photo5.onclick = function () {
+    photo5.setAttribute("id", "photo5Scale");
+}
+
+photo5.ondblclick = function () {
+    photo5.setAttribute("id", "photo5")
+}
+
+photo6.onclick = function () {
+    photo6.setAttribute("id", "photo6Scale");
+}
+
+photo6.ondblclick = function () {
+    photo6.setAttribute("id", "photo6")
+}
+
+photo7.onclick = function () {
+    photo7.setAttribute("id", "photo7Scale");
+}
+
+photo7.ondblclick = function () {
+    photo7.setAttribute("id", "photo7")
+}
+
+photo8.onclick = function () {
+    photo8.setAttribute("id", "photo8Scale");
+}
+
+photo8.ondblclick = function () {
+    photo8.setAttribute("id", "photo8")
+}
+
+photo9.onclick = function () {
+    photo9.setAttribute("id", "photo9Scale");
+}
+
+photo9.ondblclick = function () {
+    photo9.setAttribute("id", "photo9")
+}
